cae335c403dcd120e2af5c3e7290cd5645d83162
codefingas <udohezekiel34@yahoo.com>
2021-01-22 05:04:53 +0100

    lesson: annotations with variables - object literal annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,29 @@
+let apple : number = 5;
+let speed : string = 'fast';
+let hasName : boolean = true;
+
+let nothingMuch : null = null;
+let nothing : undefined = undefined;
+
+//Built-in objects
+let now : Date = new Date();
+
+//Arrays
+let colors : string[] = ['red', 'yellow', 'green', 'blue'];
+let truths : boolean[] = [true, false, true, false];
+let numbers : number[] = [1, 2, 3, 4, 5, 6, 7];
+
+//Classes
+class Car {
+
+};
+
+let car : Car = new Car();
+
+
+
+//Object literals
+let point : {x: number ; y: number} = {
+    x: 10,
+    y: 20
+};
