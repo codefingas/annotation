63882dca0fdd34905c31562f42c094155480f639
codefingas <udohezekiel34@yahoo.com>
2021-03-19 11:33:35 +0100

    update: created tuples

---
+++ annotations/tuples.ts
@@ -0,0 +1,6 @@
+//type alias
+type Drink = [string, boolean, number];
+
+const pepsi : Drink = ['brown', true, 40];
+const sprite : Drink = ['clear', true, 40];
+const tea : Drink = ['brown', false, 0];
