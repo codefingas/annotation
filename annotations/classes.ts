60b979f7e13ad03a5ac539eda1ee2f9f50ab6066
codefingas <udohezekiel34@yahoo.com>
2021-03-19 18:26:37 +0100

    update: added classes

---
+++ annotations/classes.ts
@@ -0,0 +1,28 @@
+class Vehicle {
+
+    constructor(public color: string){
+
+    }
+
+    protected honk(): void {
+        console.log('Beep Beep');
+    };
+};
+
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+// class Car extends Vehicle {
+//     private drive(): void {
+//         console.log('Vroom');
+//     }
+
+//     startdrivingProcess(){
+//         this.drive();
+//         this.honk();
+//     }
+// }
+
+// const corrolla = new Car();
+
+// corrolla.startdrivingProcess();
